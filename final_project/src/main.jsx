b493006e93b600e1ee20a5a304c37ca4592755ff
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-04-13 18:39:47 +0300

    changes in the procurement .jsx, useApi,main.jsx and installation of some dependencies

--- final_project/src/main.jsx
+++ final_project/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,5 +14,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </ErrorBoundary>
     </AuthProvider>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>
 );
